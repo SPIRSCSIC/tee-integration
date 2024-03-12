f6e2be56c3d3de76033e67fc9c639f3c21ff48ee
Sergio Chica <sergio.chica@csic.es>
2024-03-12 14:55:42 +0100

    Fixed crypto.sh

--- scripts/crypto.sh
+++ scripts/crypto.sh
@@ -13,25 +13,18 @@ OU="PoC"
 
 for org in "$@"; do
     mkdir -p "$org"
-    cd "$org"
-
     # Generate private key for CA
-    openssl genrsa -out ca.key 2048
-
+    openssl genrsa -out $org/ca.key 2048
     # Create a self signed certificate to be used for signing client certificates.
-    openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}CA"
-
+    openssl req -x509 -new -nodes -key $org/ca.key -sha256 -days 1024 -out $org/ca.crt -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}CA"
     # Generate private keys for the certificates
-    openssl genrsa -out usr1.key 2048
-    openssl genrsa -out usr2.key 2048
-
+    openssl genrsa -out $org/usr1.key 2048
+    openssl genrsa -out $org/usr2.key 2048
     # Generate a Certificate Signing Request (CSR) for each private key
-    openssl req -new -key usr1.key -out usr1.csr -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}User1"
-    openssl req -new -key usr2.key -out usr2.csr -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}User2"
-
+    openssl req -new -key $org/usr1.key -out $org/usr1.csr -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}User1"
+    openssl req -new -key $org/usr2.key -out $org/usr2.csr -subj "/C=$CT/ST=$ST/L=$LC/O=$org/OU=$OU/CN=${org}User2"
     # Use the CA to sign the certificates
-    openssl x509 -req -in usr1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out usr1.crt -days 500 -sha256
-    openssl x509 -req -in usr2.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out usr2.crt -days 500 -sha256
-    cd ..
-    cat $org/ca.crt >> chain.pem
+    openssl x509 -req -in $org/usr1.csr -CA $org/ca.crt -CAkey $org/ca.key -CAcreateserial -out $org/usr1.crt -days 500 -sha256
+    openssl x509 -req -in $org/usr2.csr -CA $org/ca.crt -CAkey $org/ca.key -CAcreateserial -out $org/usr2.crt -days 500 -sha256
 done
+eval "cat {$(IFS=','; echo "$*")}/ca.crt" > chain.pem
